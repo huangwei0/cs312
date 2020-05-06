dfb77bcbff39d8f298d2b5f564140046514590c3
huangwei0 <40774443+huangwei0@users.noreply.github.com>
2020-05-05 21:46:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ http.createServer(function (request, response)
 {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('return Docke file\n');
+  import response.end from './Dockerfile.NJSweb'
 }).listen(8080);
 console.log('Server started');
