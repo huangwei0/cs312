01c0c3750f2d9a5dd35e265530a85157b1a53fcf
huangwei0 <40774443+huangwei0@users.noreply.github.com>
2020-05-26 15:30:42 -0700

    initial common

---
+++ Ansible/script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -e ~/.ssh/id_rsa.pub ]
+then
+	echo -e "\e[1;36mSkipping ssh key generation (file exists).\e[0m"
+	echo
+	echo
+	sleep 3
+else
+	echo -e "\e[1;32mGenerating ssh key...\e[0m"
+	ssh-keygen -t rsa
+	$not_my_first_time=1
+fi
+
+echo -e "\e[1;32mCopying the ssh key to Alpine VM's\e[0m"
+ssh-copy-id root@192.168.1.23
+ssh-copy-id root@192.168.1.24
+ssh-copy-id root@192.168.1.25
+ssh-copy-id root@192.168.1.26
+
+echo -e "\e[1;32mConfiguring Ansible Playbook\e[0m"
+ansible-playbook webserver.yaml -i hosts.ini
+
+echo -e "\e[1;32mDO YOU EVEN CURL, BRO?\e[0m"
+curl 192.168.1.23
+sleep 2
+curl 192.168.1.24
+sleep 2
+curl 192.168.1.25
+sleep 2
+curl 192.168.1.26
